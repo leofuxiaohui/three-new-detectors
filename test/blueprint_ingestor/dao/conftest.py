75fd55d30cb835896385e6aab9ed0fdfbec30f37
leofuxiaohui <61720215+leofuxiaohui@users.noreply.github.com>
2021-03-30 17:17:43 -0700

    Delete test directory

--- test/blueprint_ingestor/dao/conftest.py
+++
@@ -1,9 +0,0 @@
-# Fixtures in here are available for all tests in any file.
-import pytest
-
-
-@pytest.fixture
-def when():
-    from mockito import when, unstub
-    yield when   # invokes the test that this fixture was passed to
-    unstub()     # once test is complete, remove mocks/stubs setup above
